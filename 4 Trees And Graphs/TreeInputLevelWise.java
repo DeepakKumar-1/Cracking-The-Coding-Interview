737b2e4be6a237d871363b42122d2c612ca95c9f
Deepak Kumar <85002425+DeepakKumar-1@users.noreply.github.com>
2022-01-26 23:30:25 +0530

    Create TreeInputLevelWise.java

---
+++ 4 Trees And Graphs/TreeInputLevelWise.java
@@ -0,0 +1,52 @@
+class BinaryTreeNode{
+    int val; 
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+
+      BinaryTreeNode(int val){
+      this.val = val;
+      this.left = null;
+      this.right = null;
+      }
+  
+}
+
+public class TreeInputLevelWise{
+
+    public static BinaryTreeNode<Integer> treeInputLevelWise(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Root Data: ");
+        int rootData = sc.nextInt();
+        if(rootData == -1){
+            return null;
+        }
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(rootData);
+        Queue<BinaryTreeNode<Integer>> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            BinaryTreeNode<Integer> root1 = queue.poll();
+            // Take Left Input
+            System.out.print("Enter Left Data: ");
+            int leftData = sc.nextInt();
+            if(leftData ==  -1){
+                root1.left = null;
+            }else{
+                root1.left = new BinaryTreeNode<>(leftData);
+                queue.add(root.left);
+            }
+            System.out.print("Enter right Data: ");
+            int rightData = sc.nextInt();
+            if(rightData ==  -1){
+                root1.right = null;
+            }else{
+                root1.right = new BinaryTreeNode<>(leftData);
+                queue.add(root.right);
+            }
+        }
+        return root;
+    }
+
+     public static void main(String []args){
+     BinaryTreeNode root = takeInputLevelWise();
+     }
+}
