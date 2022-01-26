2a92fe836805b7ae0b79d09b7c2ae48b1105ad57
Deepak Kumar <85002425+DeepakKumar-1@users.noreply.github.com>
2022-01-26 22:29:40 +0530

    Create DiameterOfTree.java

---
+++ 4 Trees And Graphs/DiameterOfTree.java
@@ -0,0 +1,62 @@
+package com.company.Trees;
+
+ class BinaryTreeNode<T>{
+    T val;
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
+    BinaryTreeNode(T val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+public class DiameterOfTree {
+    public static <T>BinaryTreeNode<T> createNode(T val){
+        return new BinaryTreeNode<>(val);
+    }
+
+    public static int height(BinaryTreeNode<Integer> root){
+        if(root == null){
+            return  0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return 1 + Integer.max(leftHeight, rightHeight);
+    }
+
+  
+    public static int diameterOfTree(BinaryTreeNode<Integer> root){
+        if(root == null){
+            return 0;
+        }
+        int opt1 = height(root.left) + height(root.right);
+        int opt2 = diameterOfTree(root.left);
+        int opt3 = diameterOfTree(root.right);
+        return Math.max(opt1, Math.max(opt2, opt3));
+    }
+
+    public static void main(String []args){
+        BinaryTreeNode<Integer> root, n1, n2, n3, n4, n5, n6, n7, n8;
+        root = createNode(8);
+        n1 = createNode(3);
+        n2 = createNode(10);
+        n3 = createNode(1);
+        n4 = createNode(6);
+        n5 = createNode(14);
+        n6 = createNode(4);
+        n7 = createNode(7);
+        n8 = createNode(13);
+        root.left = n1;
+        root.right = n2;
+        n1.left = n3;
+        n1.right = n4;
+        n2.right = n5;
+        n4.left = n6;
+        n4.right = n7;
+        n5.left = n8;
+
+        System.out.print("Diameter of the Tree is: " + diameterOfTree(root));
+    }
+}
