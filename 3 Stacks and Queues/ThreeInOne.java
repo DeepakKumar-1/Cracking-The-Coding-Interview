951f1e57f53d0b8c353286a6306b3fc5d8e0aaed
Deepak Kumar <85002425+DeepakKumar-1@users.noreply.github.com>
2022-01-30 13:09:13 +0530

    Update ThreeInOne.java

--- 3 Stacks and Queues/ThreeInOne.java
+++ 3 Stacks and Queues/ThreeInOne.java
@@ -1,1 +1,55 @@
+package com.company;
 
+class FullStackException extends Throwable {
+}
+class EmptyStackException extends Throwable{
+}
+public class StacksAndQueues {
+    private int noOfStacks = 3;
+    private int stackCapacity ;
+    private int[]values;
+    private int[]sizes;
+    
+    public void FixedMultiStack(int stackSize){
+        stackCapacity = stackSize;
+        values = new int[stackSize * noOfStacks];
+        sizes = new int[noOfStacks];
+    }
+    
+    // Push the Value in the Stack
+    public void push(int stackNum, int value) throws FullStackException{
+        if(isFull(stackNum)){
+            System.out.println("Stack is Full");
+            throw new FullStackException();
+        }
+        sizes[stackNum]++;
+        values[indexOfTop(stackNum)] = value;
+    }
+    
+    // Pop the Element
+    public int pop(int stackNum) throws EmptyStackException{
+        if(isEmpty(stackNum)){
+            System.out.println("Stack is Empty");
+            throw new EmptyStackException();
+        } 
+        
+        int topIndex = indexOfTop(stackNum);
+        int value = values[topIndex];
+        values[topIndex]--;
+        return value;
+    }
+    
+    public boolean isEmpty(int stackNum){
+        return sizes[stackNum] == 0;
+    }
+    
+    public boolean isFull(int stackNum){
+        return sizes[stackNum] == stackCapacity;
+    }
+    
+    public int indexOfTop(int stackNum){
+        int offset = stackNum*stackCapacity;
+        int size = sizes[stackNum];
+        return offset + size - 1;
+    }
+}
